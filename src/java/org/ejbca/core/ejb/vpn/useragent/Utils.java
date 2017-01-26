b61034099475cb807df0546a3f718e096d6f82c1
Dusan Klinec <dusan.klinec@gmail.com>
2017-01-26 15:27:34 +0100

    vpn: user agent processing utils added to common web lib

---
+++ src/java/org/ejbca/core/ejb/vpn/useragent/Utils.java
@@ -0,0 +1,26 @@
+package org.ejbca.core.ejb.vpn.useragent;
+
+/**
+ * @author harald
+ */
+public class Utils {
+    static String[] toLowerCase(String[] strArr) {
+        if (strArr == null) return null;
+        String[] res = new String[strArr.length];
+        for (int i=0; i<strArr.length; i++) {
+            res[i] = strArr[i].toLowerCase();
+        }
+        return res;
+    }
+
+    static boolean contains(String str, String[] strArr) {
+        if (strArr == null)
+            return false;
+        for (String arrStr : strArr) {
+            if (str.contains(arrStr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
